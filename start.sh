fc6b4ae38e418316e891ff41297a0abc5de0e1c8
xotosphere <xotosphere@gmail.com>
2022-04-24 13:07:01 +0200

    added changes

--- start.sh
+++ start.sh
@@ -1,45 +1,47 @@
 ######################################################
 
 # PROFILE
-export EMAIL="xotomachine@gmail.com"
-export NAME="xotomachine"
-export USER_NAME="xotomachine"
-export USER_GROUP="machine"
+# Force encoding
+echo 'EMAIL=xotomachine@gmail.com' >> /etc/environment
+echo 'NAME=xotomachine' >> /etc/environment
+echo 'USER_NAME=xotomachine' >> /etc/environment
+echo 'USER_GROUP=machine' >> /etc/environment
 
 # VERSIONS
-export NVM_VERSION="0.33.2"
-export NEOVIM_VERSION="0.7.0"
-export YARN_VERSION="1.8.0"
-export POSTGRES_VERSION="12"
-export JAVA_VERSION="11"
-export RUBBY_VERSION="2.1.1"
-export NERDS_FONT_VERSION="2.1.0"
-export FZF_VERSION="0.21.1"
-export GITSTATUS_VERSION="1.0.0"
+echo 'NVM_VERSION=0.33.2' >> /etc/environment
+echo 'NEOVIM_VERSION=0.7.0' >> /etc/environment
+echo 'YARN_VERSION=1.8.0' >> /etc/environment
+echo 'POSTGRES_VERSION=12' >> /etc/environment
+echo 'JAVA_VERSION=11' >> /etc/environment
+echo 'RUBBY_VERSION=2.1.1' >> /etc/environment
+echo 'NERDS_FONT_VERSION=2.1.0' >> /etc/environment
+echo 'FZF_VERSION=0.21.1' >> /etc/environment
+echo 'GITSTATUS_VERSION=1.0.0' >> /etc/environment
 
 # ENVIRONMENT VARS
-export DOTFILE_REPO="https://github.com/xotomachine/xotomachine-dotfiles.git"
+echo 'DOTFILE_REPO=https://github.com/xotomachine/xotomachine-dotfiles.git' >> /etc/environment
 
 ######################################################
 
-chmod a+x ./setup/*.sh
+chmod a+x /xotomachine-docker/packages/xotomachine-script/setup/*.sh
 
-sudo sh ./setup/root.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/root.sh
 
-sudo su - $USER_NAME
+su - $USER_NAME
 cd ${USER_HOME}
 
-sudo sh ./setup/apts.sh
-sudo sh ./setup/language.sh
-sudo sh ./setup/database.sh
-sudo sh ./setup/zsh.sh
-sudo sh ./setup/dotfiles.sh
-sudo sh ./setup/package.sh
-sudo sh ./setup/ccat.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/apts.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/language.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/database.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/zsh.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/dotfiles.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/package.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/ccat.sh
 
-cp -g $USER_GROUP -o $USER_NAME ./setup/ccat /usr/local/bin/
+cp -g $USER_GROUP -o $USER_NAME /xotomachine-docker/packages/xotomachine-script/setup/ccat /usr/local/bin/
 
-sudo sh ./setup/clean.sh
+sudo sh /xotomachine-docker/packages/xotomachine-script/setup/clean.sh
+
 zsh
 
 ######################################################
