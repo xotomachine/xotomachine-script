8a3016be23036d4c42d234c933965e6fcd2e4d2f
xotosphere <xotosphere@gmail.com>
2022-04-24 15:38:46 +0200

    latest edit: gitu.all -> update

--- root-install.sh
+++ root-install.sh
@@ -4,33 +4,11 @@
 
 ######################################################
 
-# PROFILE
-echo 'EMAIL=xotomachine@gmail.com' >> /etc/environment
-echo 'NAME=xotomachine' >> /etc/environment
-echo 'USER_NAME=xotomachine' >> /etc/environment
-echo 'USER_GROUP=machine' >> /etc/environment
-
-
-echo 'USER_HOME=/home/xotomachine' >> /etc/environment
-echo 'ZSH_CUSTOM=/home/xotomachine/.oh-my-zsh/custom' >> /etc/environment
-
-# VERSIONS
-echo 'NVM_VERSION=0.33.2' >> /etc/environment
-echo 'NEOVIM_VERSION=0.7.0' >> /etc/environment
-echo 'YARN_VERSION=1.8.0' >> /etc/environment
-echo 'POSTGRES_VERSION=12' >> /etc/environment
-echo 'JAVA_VERSION=11' >> /etc/environment
-echo 'RUBBY_VERSION=2.1.1' >> /etc/environment
-echo 'NERDS_FONT_VERSION=2.1.0' >> /etc/environment
-echo 'FZF_VERSION=0.21.1' >> /etc/environment
-echo 'GITSTATUS_VERSION=1.0.0' >> /etc/environment
-
-# ENVIRONMENT VARS
-echo 'DOTFILE_REPO=https://github.com/xotomachine/xotomachine-dotfiles.git' >> /etc/environment
+sudo sh /xotomachine-docker/packages/xotomachine-script/env.sh
+sudo chmod a+x /xotomachine-docker/packages/xotomachine-script/*.sh
+sudo chmod a+x /xotomachine-docker/packages/xotomachine-script/setup/*.sh
 
 ######################################################
-
-sudo chmod a+x /xotomachine-docker/packages/xotomachine-script/setup/*.sh
 
 sudo sh /xotomachine-docker/packages/xotomachine-script/setup/root.sh
 
@@ -38,5 +16,5 @@ sudo sh /xotomachine-docker/packages/xotomachine-script/setup/root.sh
 
 echo "FINISHED ~ 🍰"
 echo "Complete script by running -> "
-echo `1. su - ${USER_NAME}`
+echo "1. su - $USER_NAME"
 echo "2. sh /xotomachine-docker/packages/xotomachine-script/user-install.sh"
--- user-install.sh
+++ user-install.sh
@@ -5,6 +5,12 @@
 
 cd ~
 
+sudo sh /xotomachine-docker/packages/xotomachine-script/env.sh
+sudo chmod a+x /xotomachine-docker/packages/xotomachine-script/*.sh
+sudo chmod a+x /xotomachine-docker/packages/xotomachine-script/setup/*.sh
+
+######################################################
+
 sudo sh /xotomachine-docker/packages/xotomachine-script/setup/apts.sh
 sudo sh /xotomachine-docker/packages/xotomachine-script/setup/language.sh
 sudo sh /xotomachine-docker/packages/xotomachine-script/setup/database.sh
@@ -16,8 +22,5 @@ sudo sh /xotomachine-docker/packages/xotomachine-script/setup/ccat.sh
 cp -g $USER_GROUP -o $USER_NAME /xotomachine-docker/packages/xotomachine-script/setup/ccat /usr/local/bin/
 
 sudo sh /xotomachine-docker/packages/xotomachine-script/setup/clean.sh
-sudo rm -rf /etc/environment
-
-zsh
 
 ######################################################
