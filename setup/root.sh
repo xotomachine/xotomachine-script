b5d6049ec3da681654b5bfe232f343e32f55d6c0
xotosphere <xotosphere@gmail.com>
2022-04-25 19:39:28 +0200

    latest edit: gitu.all -> update

--- setup/root.sh
+++ setup/root.sh
@@ -19,12 +19,6 @@ set -ex &&
     peco \
     apt-transport-https
 
-###################################### ROOTS
-
-groupadd $USER_GROUP &&
-  adduser --quiet --disabled-password --ingroup $USER_GROUP --gecos 'Zsh User' $USER_NAME &&
-  adduser $USER_NAME sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
-
 ###################################### FONTS
 
 FONT_DIR=/usr/local/share/fonts &&
